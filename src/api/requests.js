27c6fc15f2251ed5c209a161fdb2c0fb2c8272f8
Stanislav Kucherenko <svkwork1@gmail.com>
2025-10-21 15:42:14 +0200

    feat: add json-server for local API testing and create initial db.json with user data
    - Added .env file to configure JSON_SERVER_API endpoint
    - Updated package.json and package-lock.json to include json-server dependency
    - Created requests.js to demonstrate API requests using axios
    - Added db.json to define initial user data for json-server

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,6 +1,18 @@
 import 'axios';
+import { JSON_SERVER_API } from '.env';
 
 const axios = require('axios');
+const API = JSON_SERVER_API;
+// Endpoints
+// users list -> users
+
+// * Test GET request
+axios({
+  method: 'get',
+  url: API + users,
+});
+
+// const axios = require('axios');
 // POST
 // GET
 // UPDATE
